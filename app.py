78e01fea08e325ccaf02840848490fdf6c9e89dd
ykadir42 <ykadir42@gmail.com>
2017-11-26 03:22:21 -0500

    Finished translation function, implemented translation into comic generator

--- app.py
+++ app.py
@@ -1,37 +1,50 @@
 import urllib2
 import json, urllib2
 from flask import Flask, render_template
-import requests
+from util import translate
+#import requests
 
 
 app = Flask(__name__)
 
-#-------------------------------GETTY------------------------------------------------
-getty_key="q7pua4pkzwvj26yakgvnaxvj"
-getty_secret_key="SUmeJueqYaAWVuCFQkCypEepBaUMw4E35j2jB3gGsWayy"
+#-------------------------------TRANSLATIONS-----------------------------------------
+@app.route('/comic')
+def comic():
+	norm_phrases = ["We found them. What do you mean?", "What a strange person. Burn her anyway!", "Well, we did do the nose.", "You can't expect to wield supreme power just 'cause some watery tart threw a sword at you!"]
+	silly_phrases = ["", "", "", ""]
+	for i in range(4):
+		silly_phrase = [norm_phrases[i], "en"]
+		for j in range(15):
+			silly_phrase = translate.translate(silly_phrase)
+		silly_phrases[i] = silly_phrase[0]
+	return render_template("comic.html", norm_phrase1 = norm_phrases[0], norm_phrase2 = norm_phrases[1], norm_phrase3 = norm_phrases[2], norm_phrase4 = norm_phrases[3], silly_phrase1 = silly_phrases[0], silly_phrase2 = silly_phrases[1], silly_phrase3 = silly_phrases[2], silly_phrase4 = silly_phrases[3])
 
-headers = { 'Api-Key': 'q7pua4pkzwvj26yakgvnaxvj'}
-params = {'phrase': 'kitties'}
-url = 'https://api.gettyimages.com/v3/search/images'
-resp= requests.get(url, headers=headers, params=params)
-result = resp.json()
-
-
-ret=[]
-for i in range(10):
-    d = {}
-    d['title'] = result['images'][i]['title']
-    inner_dict = result['images'][i]['display_sizes'][0]
-    d['uri'] = inner_dict['uri']
-    d['caption']= result['images'][i]['caption']
-    ret.append(d)
-
-#print ret
-
-@app.route('/')
-def getty():
-    return render_template('index.html', ret=ret)
-
+# #-------------------------------GETTY------------------------------------------------
+# getty_key="q7pua4pkzwvj26yakgvnaxvj"
+# getty_secret_key="SUmeJueqYaAWVuCFQkCypEepBaUMw4E35j2jB3gGsWayy"
+#
+# headers = { 'Api-Key': 'q7pua4pkzwvj26yakgvnaxvj'}
+# params = {'phrase': 'kitties'}
+# url = 'https://api.gettyimages.com/v3/search/images'
+# resp= requests.get(url, headers=headers, params=params)
+# result = resp.json()
+#
+#
+# ret=[]
+# for i in range(10):
+#     d = {}
+#     d['title'] = result['images'][i]['title']
+#     inner_dict = result['images'][i]['display_sizes'][0]
+#     d['uri'] = inner_dict['uri']
+#     d['caption']= result['images'][i]['caption']
+#     ret.append(d)
+#
+# #print ret
+#
+# @app.route('/')
+# def getty():
+#     return render_template('index.html', ret=ret)
+#
 
 if __name__ == "__main__":
     app.debug = True
--- util/translate.py
+++ util/translate.py
@@ -1,28 +1,40 @@
 from flask import Flask, render_template
-import urllib2
+import urllib, urllib2
 import json
+import random
 
-#creates the list of languages translator supports
-def get_langs():
-    uResp = urllib2.urlopen('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20171114T234337Z.0a446decad58ac47.3d65b0d73d70831cbeb0a6591eede45906e8bac8&text=' + text + '&lang=');
-	data = uResp.read()
-	d = json.loads(data)
-    return d
+key = "translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20171114T234337Z.0a446decad58ac47.3d65b0d73d70831cbeb0a6591eede45906e8bac8"
 
 #sets a variable to the list of languages
-langs = get_langs()
+#langs = ["az", "sq", "am", "en", "ar", "hy", "af", "eu", "ba", "be", "bn", "my", "bg", "bs", "cy", "hu", "vi", "ht", "gl", "nl", "mrj", "el", "ka", "gu", "da", "he", "yi", "id", "ga", "it", "is", "es", "kk", "kn", "ca", "ky", "zh", "ko", "xh", "km", "lo", "la", "lv", "lt", "lb", "mg", "ms", "ml", "mt", "mk", "mi", "mr", "mhr", "mn", "de", "ne", "no", "pa", "pap", "fa", "pl", "pt", "ro", "ru", "ceb", "sr", "si", "sk", "sl", "sw", "su", "tg", "th", "tl", "ta", "tt", "te", "tr", "udm", "uz", "uk", "ur", "fi", "fr", "hi", "hr", "cs", "sv", "gd", "et", "eo", "jv", "ja"]
 
+langs = ["en"]
 
 #returns a random language
 def rand_lang():
-    return langs[rand()]
+	return langs[random.randint(0, len(langs)-1)]
 
-#Takes a tuple of size two with the first element being the text and the second being the language it is in.
+#takes a list of size two with the first element being the text and the second being the language it is in.
 def translate(text):
-    uResp = urllib2.urlopen('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20171114T234337Z.0a446decad58ac47.3d65b0d73d70831cbeb0a6591eede45906e8bac8&text=' + text[0] + '&lang=' + text[1] + '-' + rand_lang());
+	print "text[0]: " + text[0]
+	print "text[1]: " + text[1]
+	cmd = "https://" + key + '&text=' + urllib.quote(text[0]) + '&lang=' + urllib.quote(text[1]) + '-' + urllib.quote(rand_lang())
+	print " " + cmd + " "
+	uResp = urllib2.urlopen(cmd)
+	#print "success"
 	data = uResp.read()
 	d = json.loads(data)
 	print d
-	lang = d["lang"]
-	text = d["text"]
-	return (text, lang)
+	if d["code"] != 200:
+		print "API error code" + d["code"]
+		return text
+	lang = d["lang"].split("-")[1]
+	print "lang: " + lang
+	text = d["text"][0]
+	print text
+	print lang
+	return [text, lang]
+
+#for testing purposes
+if __name__ == "__main__":
+	translate(["We found them. What do you mean? What a strange person. Burn her anyway! Well, we did do the nose. You can't expect to wield supreme power just 'cause some watery tart threw a sword at you!", "en"])
