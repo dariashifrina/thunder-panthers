ffe7b06ea2b49d4282aa5e94469fadd0fbc82302
Karina Ionkina <kionkina@stuy.edu>
2017-11-28 13:19:29 -0500

    app py work

--- app.py
+++ app.py
@@ -20,40 +20,42 @@ def comic():
 	silly_phrases = ["", "", "", ""]
 	for i in range(4):
 		silly_phrase = [datamuse.new_sent(norm_phrases[i]), "en"]
-		for j in range(20):
+		for j in range(3):
 			silly_phrase = translate.translate(silly_phrase)
 		silly_phrases[i] = datamuse.new_sent(translate.to_english(silly_phrase)[0])
-	return render_template("comic.html", norm_phrase1 = norm_phrases[0], norm_phrase2 = norm_phrases[1], norm_phrase3 = norm_phrases[2], norm_phrase4 = norm_phrases[3], silly_phrase1 = silly_phrases[0], silly_phrase2 = silly_phrases[1], silly_phrase3 = silly_phrases[2], silly_phrase4 = silly_phrases[3])
+	return render_template("comic.html", norm_phrase1 = norm_phrases[0], norm_phrase2 = norm_phrases[1], norm_phrase3 = norm_phrases[2], norm_phrase4 = norm_phrases[3], silly_phrase1 = silly_phrases[0], silly_phrase2 = silly_phrases[1], silly_phrase3 = silly_phrases[2], silly_phrase4 = silly_phrases[3], norm_url1 = img_url(norm_phrases[0])['uri'], norm_url2 = img_url(norm_phrases[1])['uri'], norm_url3 = img_url(norm_phrases[2])['uri'], norm_url4 = img_url(norm_phrases[3])['uri'], silly_url1 = img_url(silly_phrases[0]), silly_url2 = img_url(silly_phrases[1]), silly_url3 = img_url(silly_phrases[2]), silly_url4 = img_url(silly_phrases[3]) )
 
 # #-------------------------------GETTY------------------------------------------------
 
-
 sentence = "Jawad plays hockey at batter park."
-phrase = datamuse.img_text(sentence)
 
-getty_key="q7pua4pkzwvj26yakgvnaxvj"
-getty_secret_key="SUmeJueqYaAWVuCFQkCypEepBaUMw4E35j2jB3gGsWayy"
+def img_url(sentence):
 
-headers = { 'Api-Key': 'q7pua4pkzwvj26yakgvnaxvj'}
-params = {'phrase': phrase}
-url = 'https://api.gettyimages.com/v3/search/images'
-resp= requests.get(url, headers=headers, params=params)
-result = resp.json()
+	phrase = datamuse.img_text(sentence)
 
+	getty_key="q7pua4pkzwvj26yakgvnaxvj"
+	getty_secret_key="SUmeJueqYaAWVuCFQkCypEepBaUMw4E35j2jB3gGsWayy"
 
-ret=[]
-for i in range(10):
+	headers = { 'Api-Key': 'q7pua4pkzwvj26yakgvnaxvj'}
+	params = {'phrase': phrase}
+	url = 'https://api.gettyimages.com/v3/search/images'
+	resp= requests.get(url, headers=headers, params=params)
+	result = resp.json()
 	d = {}
-	d['title'] = result['images'][i]['title']
-	inner_dict = result['images'][i]['display_sizes'][0]
-	d['uri'] = inner_dict['uri']
-	d['caption']= result['images'][i]['caption']
-	ret.append(d)
+	ret = []
+	if result['images'] != []:
+		inner_dict = result['images'][0]['display_sizes'][0]
+		d['uri'] = inner_dict['uri']
+	else: 
+		d['uri'] = "https://i.ytimg.com/vi/K4zm30yeHHE/maxresdefault.jpg"
+
+	
+	return d
 
 
 @app.route('/getty')
 def getty():
-	return render_template('getty.html', sentence=sentence, phrase=phrase, ret=ret)
+	return render_template('getty.html', sentence=sentence, ret= img_url(sentence))
 #
 
 if __name__ == "__main__":
