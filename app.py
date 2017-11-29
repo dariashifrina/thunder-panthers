9a517460c6c48773ba8e7097de1404e80696f3c0
ykadir42 <ykadir42@gmail.com>
2017-11-28 20:14:22 -0500

    try-except loop

--- app.py
+++ app.py
@@ -34,12 +34,16 @@ def comic():
 	norm_phrases = ["Spider-Man went to the store to buy eggs and milk.", "Mr Brown was also at the store because he was buying a computer.", "He saw Spider-Man and was surprised.", "Then they both left the store."]
 	silly_phrases = ["", "", "", ""]
 	for i in range(4):
-		silly_phrase = [datamuse.new_sent(norm_phrases[i]), "en"]
-		for j in range(3):
-			silly_phrase = translate.translate(silly_phrase)
-		silly_phrases[i] = datamuse.new_sent(translate.to_english(silly_phrase)[0])
+		while True:
+			try:
+				silly_phrase = [datamuse.new_sent(norm_phrases[i]), "en"]
+				for j in range(3):
+					silly_phrase = translate.translate(silly_phrase)
+				silly_phrases[i] = datamuse.new_sent(translate.to_english(silly_phrase)[0])
+				break
+			except IndexError:
+				pass
 	return render_template("comic.html", norm_phrase1 = norm_phrases[0], norm_phrase2 = norm_phrases[1], norm_phrase3 = norm_phrases[2], norm_phrase4 = norm_phrases[3], silly_phrase1 = silly_phrases[0], silly_phrase2 = silly_phrases[1], silly_phrase3 = silly_phrases[2], silly_phrase4 = silly_phrases[3], norm_url1 = img_url(norm_phrases[0])['uri'], norm_url2 = img_url(norm_phrases[1])['uri'], norm_url3 = img_url(norm_phrases[2])['uri'], norm_url4 = img_url(norm_phrases[3])['uri'], silly_url1 = img_url(silly_phrases[0]), silly_url2 = img_url(silly_phrases[1]), silly_url3 = img_url(silly_phrases[2]), silly_url4 = img_url(silly_phrases[3]) )
-
 # #-------------------------------GETTY------------------------------------------------
 
 sentence = "Jawad plays hockey at batter park."
@@ -61,10 +65,10 @@ def img_url(sentence):
 	if result['images'] != []:
 		inner_dict = result['images'][0]['display_sizes'][0]
 		d['uri'] = inner_dict['uri']
-	else: 
+	else:
 		d['uri'] = "https://i.ytimg.com/vi/K4zm30yeHHE/maxresdefault.jpg"
 
-	
+
 	return d
 
 
